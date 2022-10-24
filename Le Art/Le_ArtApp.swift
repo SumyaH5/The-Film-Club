a1baf1b4e9adc3925a5856ad98b2dc927d78350d
SumyaH5 <108182578+SumyaH5@users.noreply.github.com>
2022-10-24 11:06:10 -0400

    Initial Commit

---
+++ Le Art/Le_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Le_ArtApp.swift
+//  Le Art
+//
+//  Created by Sumya Hoque on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Le_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
